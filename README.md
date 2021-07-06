557ee189e467d05ced6e522b4d8b735df6f92172
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-07-06 20:38:01 +0100

    cleaned up naming conventions

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Check concurrent Stream Service
 
+simple service to check a given's users concurrent streams
+
+## to install
 npm install
 
+## to start
 npm run start
 
+## to run tests
 npm run test
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,6 +3,7 @@ import {app}from '../src/app'
 
 it('returns success message when checking for a user with no existing concurrent streams running', async()=> {
     const res1 = await request(app).get('/user1')
+    expect(res1.status).toEqual(200)
     expect(res1.body).toEqual('Success! able to watch stream')
 })
 
@@ -15,7 +16,8 @@ it('returns error message when checking for a user with more than 3 existing con
       ])
 
     const [res1, res2, res3, res4] = resData
-    expect(res4.body).toEqual('Error: Too many cuncurrent streams')
+    expect(res4.status).toEqual(403)
+    expect(res4.body).toEqual('Forbidden: Too many cuncurrent streams')
 })
 
 
@@ -29,5 +31,6 @@ it('returns success response for user2 profile', async()=> {
       ])
 
     const [res1, res2, res3, res4, res5] = resData
+    expect(res5.status).toEqual(200)
     expect(res5.body).toEqual('Success! able to watch stream')
 })
