29ebdb2036aa129fbcc2f15eb0f831066f689326
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-07-15 18:53:38 +0100

    added lint
    added lint file

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -5,6 +5,7 @@ const checkStreamRouter = Router()
 
 const result = new UserStreams()
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 checkStreamRouter.get('/:userId', async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
     const { userId } = req.params
     if (result.getUserStreamCount(userId) >= 3) return res.status(403).json('Forbidden: Too many cuncurrent streams')
